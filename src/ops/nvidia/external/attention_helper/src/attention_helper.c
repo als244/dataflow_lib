679e7c82e82125ba05365eca7c77bdb91deb2e06
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-03-27 19:21:21 -0400

    Finished fwd attention helper. Will add to transformer ops and test correctness of full transformer block. Then will add wrapper for attn backward and start testing bwd pass...git status!

---
+++ src/ops/nvidia/external/attention_helper/src/attention_helper.c
@@ -0,0 +1,71 @@
+#include "attention_helper.h"
+
+int flash3_attention_fwd(Dataflow_Handle * dataflow_handle, int stream_id, Op * op, void * op_extra) {
+	
+	/*
+	int flash3_fwd_wrapper(CUstream stream, int arch, int num_sm
+                        int num_seqs, int total_q, int total_k, 
+                        int * cum_q_seqlens, int max_seqlen_q,
+                        int * k_seqlens, int max_seqlen_k, 
+                        int flash_dtype_as_int, 
+                        int num_q_heads, int num_kv_heads, int head_dim, 
+                        void * x_q, void * x_k, void * x_v, void * x_attn_out, void * softmax_lse, 
+                        void * attn_workspace,
+                        ) */
+
+	Cuda_Device_Info * device_info = (Cuda_Device_Info *) dataflow_handle -> device_info;
+
+	int arch = device_info -> arch_num;
+	int sm_count = device_info -> sm_count;
+
+	CUstream * streams = (CUstream *) (dataflow_handle -> streams);
+
+	CUstream stream = streams[stream_id];
+
+	void ** op_args = op -> op_args;
+
+	// Follow same paradigm as submitting to command queue
+	// pass in pointers to each of the arguments...
+
+	// so now we can deference them...
+
+	int num_seqs = *((int *)(op_args[0]));
+	int total_q = *((int *)(op_args[1]));
+	int total_k = *((int *)(op_args[2]));
+	int * cum_q_seqlens = *((int **) op_args[3]);
+	int max_seqlen_q = *((int *)(op_args[4]));
+	int * k_seqlens = *((int **) op_args[5]);
+	int max_seqlen_k = *((int *)(op_args[6]));
+	int flash_dtype_as_int = *((int *)(op_args[7]));
+	int num_q_heads = *((int *)(op_args[8]));
+	int num_kv_heads = *((int *)(op_args[9]));
+	int head_dim = *((int *)(op_args[10]));
+
+	void * x_q = *((void **) op_args[11]);
+	void * x_k = *((void **) op_args[12]);
+	void * x_v = *((void **) op_args[13]);
+
+	void * x_attn_out = *((void **) op_args[14]);
+	void * softmax_lse = *((void **) op_args[15]);
+
+	void * attn_workspace = *((void **) op_args[16]);
+
+	int ret = flash3_fwd_wrapper(stream, arch, sm_count,
+									num_seqs, total_q, total_k,
+									cum_q_seqlens, max_seqlen_q,
+									k_seqlens, max_seqlen_k,
+									flash_dtype_as_int,
+									num_q_heads, num_kv_heads, head_dim,
+									x_q, x_k, x_v,
+									x_attn_out, softmax_lse,
+									attn_workspace);
+
+
+	return ret;
+}
+
+
+int flash3_attention_bwd(Dataflow_Handle * dataflow_handle, int stream_id, Op * op, void * op_extra) {
+
+	return 0;
+}
