f48612dc33e334c6828b738ab827f44ba9cd7df4
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-03-28 20:35:31 -0400

    Made paths to key objs relative

--- src/dataflow_handle/nvidia/test/test_cuda_dataflow_handle_matmul_raw.c
+++ src/dataflow_handle/nvidia/test/test_cuda_dataflow_handle_matmul_raw.c
@@ -20,9 +20,9 @@ int main(int argc, char * argv[]){
 	int opt_stream_prios[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 	char * opt_stream_names[8] = {"Inbound (a)", "Compute (a)", "Outbound (a)", "Peer (a)", "Inbound (b)", "Compute (b)", "Outbound (b)", "Peer (b)"};
 	
-	char * all_function_meta_filename = "/home/shein/Documents/grad_school/research/ml_dataflow/dataflow_lib/src/ops/nvidia/lib/cuda_all_functions_meta.dat";
-	char * native_function_config_filename = "/home/shein/Documents/grad_school/research/ml_dataflow/dataflow_lib/src/ops/nvidia/lib/cuda_kernels_config.so";
-	char * native_function_lib_filename = "/home/shein/Documents/grad_school/research/ml_dataflow/dataflow_lib/src/ops/nvidia/lib/cuda_kernels.cubin";
+	char * all_function_meta_filename = "../../ops/nvidia/lib/cuda_all_functions_meta.dat";
+        char * native_function_config_filename = "../../ops/nvidia/lib/cuda_kernels_config.so";
+        char * native_function_lib_filename = "../../ops/nvidia/lib/cuda_kernels.cubin";
 
 	ret = init_cuda_dataflow_handle(&cuda_dataflow_handle, compute_type, device_id, 
 			ctx_id, ctx_flags, 
